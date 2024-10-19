fb640c3679c0588dc501c7b4a08b69ea44991815
Lasata03 <lasata.dangol03@gmail.com>
2024-10-19 09:23:57 +0545

    website created

---
+++ scroll.js
@@ -0,0 +1,6 @@
+document.querySelector('a[href="#contact"]').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.querySelector('#contact').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
